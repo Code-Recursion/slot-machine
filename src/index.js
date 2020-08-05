a7b061abc33cadd1afda602d98371f80f342af57
Ajay Singh <helloajaysingh1@gmail.com>
2020-08-05 15:45:36 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+// import App from "./App";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <>
+    <div className="cards">
+      <div className="card">
+        <img
+          src="https://fanart.tv/detailpreview/fanart/tv/277165/tvposter/silicon-valley-5509b213926fb.jpg"
+          alt="picture"
+          className="card__img"
+        />
+        <div className="card__info">
+          <span className="card__category">An Original Seiries</span>
+          <h3 className="card__title">Silicon Valley</h3>
+          <a
+            href="https://fanart.tv/series/277165/silicon-valley/"
+            target="_blank"
+          >
+            <button>Watch Now</button>
+          </a>
+        </div>
+      </div>
+    </div>
+  </>,
+  document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
