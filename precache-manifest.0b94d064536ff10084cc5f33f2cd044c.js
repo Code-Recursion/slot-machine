dd0843fcd9c2a45e40259bd8b3e9079b2525bead
Ajay Singh <helloajaysingh1@gmail.com>
2020-08-16 11:17:58 +0530

    Updates

---
+++ precache-manifest.0b94d064536ff10084cc5f33f2cd044c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a911f9418e18c3c0f038635b87bdef7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b9cb5a5b756999902d08",
+    "url": "/static/css/main.2fcbe62e.chunk.css"
+  },
+  {
+    "revision": "6b8dc912edce1cb3e0ff",
+    "url": "/static/js/2.3e2e52e3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3e2e52e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9cb5a5b756999902d08",
+    "url": "/static/js/main.37b8a7dc.chunk.js"
+  },
+  {
+    "revision": "5387050906b8ed53cb42",
+    "url": "/static/js/runtime-main.ec2d9410.js"
+  }
+]);
